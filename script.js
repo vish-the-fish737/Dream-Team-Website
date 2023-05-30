9842b0c004ecf6beb97f6e0904ba87523c2ef107
sebgutierrez <84500161+sebgutierrez@users.noreply.github.com>
2023-05-30 16:01:31 -0500

    Added mobile navigation bar

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const sections = document.querySelectorAll('section'); // select each section (page)
-const navBtns = document.querySelectorAll('nav'); // select parent container of buttons
+const navBtns = document.querySelectorAll('nav-list'); // select parent container of buttons
 const navItemBtn = document.querySelectorAll('.menu-item-container'); // select each individual buttons
 const allSections = document.querySelector('.main-content') // select all sections in webpage
+const toggle = document.querySelector('.menu-toggle'); 
+var navbars = document.querySelectorAll('.navbar');
 
 // Sections Active
 
@@ -15,15 +17,15 @@ function PageTransitions() {
       // Removes the "active" class from all buttons in the sectBtns array using the forEach method.
       navBtns.forEach((btn) => {
         btn.classList.remove('active')
-      })
+      });
 
       // Adds the "active" class to the clicked button using e.target.classList.add('active').
-      e.target.classList.add('active')
+      e.target.classList.add('active');
 
       // Removes the "active" class from all sections in the sections array using the forEach method.
       sections.forEach((section) => {
         section.classList.remove('active')
-      })
+      });
 
       // Adds the "active" class to the section with the corresponding id using 
       // document.getElementById(id).classList.add('active').
@@ -35,21 +37,28 @@ function PageTransitions() {
 
     }
   })
-}
+};
 
 PageTransitions();
-
 
 var hideScroll = window.pageYOffset;
 
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
-
-  if (hideScroll > window.pageYOffset) {
-    document.getElementById("nav").style.top = "0";
-  } else {
-    document.getElementById("nav").style.top = "-10rem";
-  }
+  const checked = document.getElementById("nav-toggle").checked;
+  
+  navbars.forEach((nav) => {
+    if(nav.style.display != "none"){
+      if (hideScroll > window.pageYOffset && checked == false) {
+        nav.style.top = "0";
+      } 
+      else {
+        if(checked == false){
+          nav.style.top = "-10rem";
+        }
+      }
+    }
+  })
 
   hideScroll = currentScrollPos;
-}
+};
